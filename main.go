899779f035b3f377795fdf4966fb8a38cbe58535
Dmitry Sakovich <sakovich.dima95@gmail.com>
2022-10-14 11:21:14 +0300

    add config file

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"log"
 	"tasktracker-api/pkg/repository"
 	router "tasktracker-api/pkg/router"
 	"tasktracker-api/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	err := getConfig()
+	if err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repository := repository.NewRepository()
 	services := service.NewService(repository)
 	router := router.NewRouter(services)
 	app := router.InitRoutes()
-	app.Run(":8080")
+	app.Run(viper.GetString("port"))
+}
+
+func getConfig() error {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	return viper.ReadInConfig()
 }
