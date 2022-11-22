997e08f2a0f9063731e448451f649e73e77dda0e
Dmitry Sakovich <Sakovich.dima95@gmail.ru>
2022-11-22 15:29:05 +0300

    migration fix

--- db/migrations/00001_create_tables.up.sql
+++ db/migrations/00001_create_tables.up.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS public.users (
 	username varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	"password" varchar(255) NOT NULL,
-	"groups" text NULL DEFAULT '[]'::text,
 	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
@@ -12,12 +11,13 @@ CREATE TABLE IF NOT EXISTS public.tasks (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	title varchar(255) NOT NULL,
 	status int4 NOT NULL DEFAULT 0,
-	created_by int4 NOT NULL DEFAULT 1,
+	created_by int4 NOT NULL DEFAULT 0,
 	priority int4 NOT NULL DEFAULT 0,
-	description varchar(1000) NULL,
-	group_id int4 NULL DEFAULT 0,
+	description varchar(1000) NOT NULL,
+	group_id int4 NOT NULL,
 	CONSTRAINT tasks_pk PRIMARY KEY (id),
-	CONSTRAINT u_id FOREIGN KEY (created_by) REFERENCES public.users(id)
+	CONSTRAINT u_id FOREIGN KEY (created_by) REFERENCES public.users(id),
+	CONSTRAINT g_id FOREIGN KEY (group_id) REFERENCES public.groups(id)
 );
 
 CREATE TABLE IF NOT EXISTS public."group" (
